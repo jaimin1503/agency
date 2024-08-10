227f620740ab2b5dbfe339bc10a7a87cf4095e05
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-08-10 11:08:31 +0530

    footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,70 +9,70 @@ const Footer = () => {
 			<div className=" flex justify-evenly w-full">
 				<div className="socials flex flex-col justify-start items-start">
 
-					<h1 className=" text-xl font-bold mb-2">
+					<h1 className=" sm:text-xl font-bold mb-2">
 						Social Links
 					</h1>
 					<div className=" flex items-center my-1">
 						<InstagramLogoIcon className="w-6 h-6" />
-						<p className="text-md mx-2">Instagram</p>
+						<p className=" text-sm sm:text-md mx-2">Instagram</p>
 					</div>
 
 					<div className=" flex items-center my-1">
 						<GitHubLogoIcon className="w-6 h-6" />
-						<p className="text-md mx-2">Github</p>
+						<p className=" text-sm sm:text-md mx-2">Github</p>
 					</div>
 
 					<div className=" flex items-center my-1">
 						<TwitterLogoIcon className="w-6 h-6" />
-						<p className="text-md mx-2">Twitter</p>
+						<p className=" text-sm sm:text-md mx-2">Twitter</p>
 					</div>
 
 					<div className=" flex items-center my-1">
 						<LinkedInLogoIcon className="w-6 h-6" />
-						<p className="text-md mx-2">LinkedIn</p>
+						<p className=" text-sm sm:text-md mx-2">LinkedIn</p>
 					</div>
 
 				</div>
 				<div className="quickLinks flex flex-col justify-start items-start">
 
-					<h1 className=" text-xl font-bold mb-2">
+					<h1 className="sm:text-xl font-bold mb-2">
 						Quick Links
 					</h1>
 
 					<div className=" flex items-center my-1">
-						<Link href={"/"} className="text-md mx-2">Home</Link>
+						<Link href={"/"} className=" text-sm sm:text-md mx-2">Home</Link>
 					</div>
 
 					<div className=" flex items-center my-1">
-						<Link href={"/about"} className="text-md mx-2">About</Link>
+						<Link href={"/about"} className=" text-sm sm:text-md mx-2">About</Link>
 					</div>
 
 					<div className=" flex items-center my-1">
-						<Link href={"/services"} className="text-md mx-2">Services</Link>
+						<Link href={"/services"} className=" text-sm sm:text-md mx-2">Services</Link>
 					</div>
 
 					<div className=" flex items-center my-1">
-						<Link href={"/contact"} className="text-md mx-2">Contact</Link>
+						<Link href={"/contact"} className=" text-sm sm:text-md mx-2">Contact</Link>
 					</div>
 				</div>
 				<div className="Contact">
 
-					<h1 className=" text-xl font-bold mb-2">
+					<h1 className=" sm:text-xl font-bold mb-2">
 						Contact Us
 					</h1>
 					<div className=" flex items-center my-1">
 						<GlobeIcon className="w-6 h-6" />
-						<Link href={"https://jaimin-15-03.netlify.app"} className="text-md mx-2">Website</Link>
+						<Link href={"https://jaimin-15-03.netlify.app"} className=" text-sm sm:text-md mx-2">Website</Link>
 					</div>
 
 					<div className=" flex items-center my-1">
 						<Mail className="w-6 h-6" />
-						<Link href={"mailto:jaiminv153@gmail.com"} className="text-md mx-2">Email</Link>
+						<Link href={"mailto:jaiminv153@gmail.com"} className=" text-sm sm:text-md mx-2">Email</Link>
 					</div>
 
 					<div className=" flex items-center my-1">
 						<ContactIcon className="w-6 h-6" />
-						<p className="text-md mx-2"> &#43; 917 123 4567</p>
+						<p className=" text-sm sm:text-md mx-2"> &#43; 917 123 4567</p>
 					</div>
 				</div>
 			</div>
